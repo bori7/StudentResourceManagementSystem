71d2ca5669279483f65a2bb370ca8aca804d38a7
boma <boma.obomanu86@gmail.com>
2022-11-28 11:18:33 +0100

    Testing For Repository and Controller

--- src/test/java/com/ecobank/srms/repository/StudentRepositoryTest.java
+++ src/test/java/com/ecobank/srms/repository/StudentRepositoryTest.java
@@ -14,18 +14,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.TemporalAccessor;
 import java.util.*;
 import java.util.logging.Logger;
 
